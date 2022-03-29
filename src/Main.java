e8564975956132322becc15e2663127a7b3dcc5d
Rimma <rimmulya19931993@gmail.com>
2022-03-29 21:21:13 +0300

    second

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,27 @@ import java.util.HashSet;
 public class Main {
     public static void main(String[] args) {
         System.out.println("HashSet");
+        System.out.println("hello world");
+        Person_1 p1 =new Person_1();
+        p1.setId(100);
+        p1.setName("John");
+
+        Person_1 p2 =new Person_1();
+        p2.setId(100);
+        p2.setName("Jonh");
+
+        System.out.println(p1.hashCode());
+        System.out.println(p2.hashCode());
+        System.out.println(p2.equals(p1));
+
+
+        System.out.println(p1.equals(p2));
+        Person_1 p3 =p2;
+        System.out.println(p2.equals(p3));
+        System.out.println("TEST for GIT");
+
+
+
 
     }
 }
